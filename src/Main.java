1ea96812db3d810d63c33c60e3ee8df4b7e5056a
vlj2007 <vlj2007@yandex.ru>
2023-10-18 21:33:56 +0700

    I have finished task2

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("ДЗ Коллекции: сеты и мапы");
         task1();
+        task2();
     }
 
     public static void task1() {
@@ -11,6 +12,17 @@ public class Main {
         for (Integer item : nums) {
             if (item % 2 != 0) {
                 System.out.println(item);
+            }
+        }
+    }
+
+    public static void task2() {
+        System.out.println("Задание 2");
+        List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+        SortedSet<Integer> nums2 = new TreeSet<>(nums);
+        for (Integer item : nums2) {
+            if (item % 2 == 0) {
+                System.out.println(item.toString());
             }
         }
     }
